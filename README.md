32345c85795ffca92cecaac57005f427b3e48e4b
Max Zakharzhevskiy <maxzakh@ucdavis.edu>
2020-03-15 20:38:58 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Description
+Learning GraphQL concepts and implementation.
+
+# Source
+
+[Building GraphQL APIs](https://css-tricks.com/get-started-building-graphql-apis-with-node/)
---
+++ src/index.js
@@ -0,0 +1,58 @@
+const { ApolloServer } = require('apollo-server');
+const typeDefs = require('./schema');
+
+let highlights = [
+    {
+        id: '1',
+        content: 'One day I will find the right words, and they will be simple.',
+        title: 'Dharma Bums',
+        author: 'Jack Kerouac'
+    },
+    {
+        id: '2',
+        content: 'In the limits of a situation there is humor, there is grace, and everything else.',
+        title: 'Arbitrary Stupid Goal',
+        author: 'Tamara Shopsin'
+    }
+];
+
+const resolvers = {
+    Query: {
+        highlights: () => highlights,
+        highlight: (parent, args) => highlights.find((item) => item.id === args.id)
+    },
+    Mutation: {
+        newHighlight: (parent, args) => {
+            const h = {
+                id: String(highlights.length + 1),
+                title: args.title || '',
+                content: args.content,
+                author: args.author || ''
+            };
+            highlights.push(h);
+            return h;
+        },
+        updateHighlight: (parent, args) => {
+            const h = highlights.find((item) => {
+                return item.id === args.id;
+            });
+            h.content = args.content;
+            return h;
+        },
+        deleteHighlight: (parent, args) => {
+            const h = highlights.find((item) => {
+                return item.id === args.id;
+            });
+            highlights = highlights.filter((item) => {
+                return item.id !== args.id;
+            });
+            return h;
+        }
+    }
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+server.listen().then(({ url }) => {
+    console.log(`Highlights is running on ${url}`);
+});
